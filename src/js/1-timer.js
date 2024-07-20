6bc090d3c40a79f0f4fdbfb2a5cead1a922e710e
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-07-20 13:59:04 +0200

    added icons for message

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,6 +3,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import iconUrl from '../img/bi-x-octagon.svg';
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -39,12 +40,110 @@ function addLeadingZero(dateObj) {
   }, {});
 }
 
+const refs = {
+    days: document.querySelector('.value[data-days]'),
+    timer: document.querySelector('.timer'),
+    hours: document.querySelector('.value[data-hours]'),
+    minutes: document.querySelector('.value[data-minutes]'),
+    seconds: document.querySelector('.value[data-seconds]'),
+    startBtn: document.querySelector('button[data-start]'),
+    picker: document.getElementById('datetime-picker'),
+    stopBtn: document.querySelector('button[data-stop]'),
+  };
+  
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = true;
+  
+  refs.startBtn.addEventListener('click', onStartButtonClick);
+  refs.stopBtn.addEventListener('click', onStopButtonClick);
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const date = new Date(selectedDates[0]);
+    if (date - new Date() > 0) {
+      userSelectedDate = date;
+      refs.startBtn.disabled = false;
+    } else {
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        messageColor: '#fafafb',
+        messageSize: '16px',
+        backgroundColor: '#fc5a5a',
+        position: 'topRight',
+        closeOnEscape: true,
+        close: true,
+        icon: 'Icomoon',
+        iconUrl: `${iconUrl}`,
+        iconColor: '#fafafb',
+      });
+    }
   },
 };
+
+const instflatpickr = flatpickr('input#datetime-picker', options);
+
+let userSelectedDate = null;
+const interval = 1000;
+let intervalId;
+
+function onStartButtonClick() {
+  updateTimer();
+  intervalId = setInterval(updateTimer, interval);
+  changeControlsStatus();
+}
+
+function onStopButtonClick() {
+  resetTimer();
+}
+
+function updateTimer() {
+  if (!userSelectedDate) return;
+
+  const restTime = userSelectedDate - new Date();
+  if (restTime <= 0) {
+    resetTimer();
+    return;
+  }
+  const formattedValues = convertMs(restTime);
+  insertDataToControls(formattedValues);
+}
+
+function insertDataToControls(dateObj) {
+  if (dateObj) {
+    const { days, hours, minutes, seconds } = dateObj;
+    if (String(days).length > 3) {
+      refs.timer.classList.add('timer-shift');
+    }
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = minutes;
+    refs.seconds.textContent = seconds;
+  } else {
+    refs.timer.classList.remove('timer-shift');
+    refs.days.textContent = '00';
+    refs.hours.textContent = '00';
+    refs.minutes.textContent = '00';
+    refs.seconds.textContent = '00';
+  }
+}
+
+function resetTimer() {
+  userSelectedDate = new Date();
+  intervalId && clearInterval(intervalId);
+  intervalId = null;
+  instflatpickr.setDate(new Date());
+  insertDataToControls();
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = true;
+  refs.picker.disabled = false;
+}
+
+function changeControlsStatus() {
+  refs.startBtn.disabled = !refs.startBtn.disabled;
+  refs.picker.disabled = !refs.picker.disabled;
+  refs.stopBtn.disabled = !refs.stopBtn.disabled;
+}
